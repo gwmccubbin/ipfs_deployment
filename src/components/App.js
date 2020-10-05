a4e858fa24730930f037946945de5ef796287d7c
gwmccubbin <gwmccubbin@gmail.com>
2020-10-05 15:49:37 -0500

    Updates project for IPFS deployment

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ class App extends Component {
                 </a>
                 <h1>Dapp University Starter Kit</h1>
                 <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
+                  This website is running on IPFS!
                 </p>
                 <a
                   className="App-link"
